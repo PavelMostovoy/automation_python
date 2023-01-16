24381ec0eb065450f67a29c04a08c2f280ef4f1c
Pavel Mostovoy <pavelm@bemobi.com>
2023-01-16 19:56:43 +0100

    practice

---
+++ practice/page_obj.py
@@ -0,0 +1,5 @@
+url = "https://wikipedia.org"
+ext = "wiki"
+info = "World War I"
+search_field_id = "searchInput"
+search_button_id = '//*[@id="search-form"]/fieldset/button/i'
--- practice/test_verden.py
+++ practice/test_verden.py
@@ -3,3 +3,25 @@
 # Find Verden apearanc on page
 # additional Find on page 11.11.18
 # additionally find time
+
+import time
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import page_obj
+
+
+
+expected_url = f'{page_obj.url}'
+
+driver = webdriver.Chrome()
+driver.get(expected_url)
+
+search_filed = driver.find_element(By.ID, page_obj.search_field_id)
+search_filed.send_keys(page_obj.info)
+# search_filed.send_keys(Keys.ENTER)
+
+driver.find_element(By.XPATH, page_obj.search_button_id).click()
+
+time.sleep(100)
