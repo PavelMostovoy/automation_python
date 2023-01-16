1d0ecd52edb4aa2659aa5de75d383f831ed95537
Pavel Mostovoy <pavelm@bemobi.com>
2023-01-16 20:18:17 +0100

    practice

--- practice/test_verden.py
+++ practice/test_verden.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-from data import Config, Wikipedia
+from page_obj import Config, Wikipedia
 
 expected = Wikipedia(Config.url)
 
