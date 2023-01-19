70bec7a3a522e9ee9dc4f934ca9a42dc1636fa02
Pavel Mostovoy <pavelm@bemobi.com>
2023-01-19 18:56:20 +0100

    pathlib

--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,19 @@
-import random
-import time
+from pathlib import Path
+
+file_name = "example.txt"
+
+current_location = Path(__file__).parent
+
+next_location = current_location.joinpath("resources").joinpath("text_documents").joinpath("new_folder").mkdir(parents=True, exist_ok=True)
+# folder_path = Path("resources").join("text_documents")
+
+print(current_location)
+print(next_location)
 
 
-st = time.time()
-time.sleep(random.randint(1,3))
-et = time.time()
 
-print(round((et - st), 2))
+#
+# full_filename = f"{folder_path}{file_name}"
+#
+# with open(full_filename, "w+", encoding="utf-8") as file:
+#     file.write("our data")
