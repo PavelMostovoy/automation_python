8414ae8c8b663463904b2ce6e1a8a70845b8a582
Pavel Mostovoy <pavel.mostovoy@gmail.com>
2023-02-23 20:18:29 +0100

    mail_example

--- practice/script.py
+++ practice/script.py
@@ -1,3 +1,4 @@
+import string
 with open("test_example.html", "rb") as f:
     data = f.read()
 
@@ -11,8 +12,19 @@ new_list = []
 for i, element in enumerate (new_data):
     if 'please use the code:' in element:
         new_list.append(element.split())
-        print(i, element)
+        # print(i, element)
+# print(new_list)
+# exit()
+for i in new_list:
+    for n, j in enumerate(i):
+        if j == 'code:':
+            try:
+                print(i[n+1])
+            except IndexError:
+                pass
+            else:
+                if i[n+1].isdigit():
+                    print(f'{i[n+1]}')
 
 
-
-print(new_list)
+# print(new_list)
